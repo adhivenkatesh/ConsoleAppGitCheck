05785241a9ef52bc0ff60880778f9e8f7c6ea86e
Adhi Venkatesh <adhivenkatesh32@gmail.com>
2021-05-08 14:06:57 +0530

    change-from MODIFIEDBRANCH-TEXT01

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppGitCheck
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World change from MODIFIED BRANCH!");
         }
     }
 }
