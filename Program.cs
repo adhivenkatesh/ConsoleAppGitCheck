06ca2c1411f5c6c6d997af92e7d4f76709ae35cb
Adhi Venkatesh <adhivenkatesh32@gmail.com>
2021-05-08 17:55:57 +0530

    Now at 5.55 pm made changes

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleAppGitCheck
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World change from MODIFIED -Data from master");
-            Console.WriteLine("Hello World change from MODIFIED - from github");
+            Console.WriteLine("Hello World change from MODIFIED - from github now at 6.55 pm");
         }
     }
 }
