8e224a48c6165be63b39260885bb9e7279b2205b
Adhi Venkatesh <adhivenkatesh32@gmail.com>
2021-05-08 13:31:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppGitCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
