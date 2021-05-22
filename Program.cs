16fb3b222bb1765cc185e1cdf64696fca9b4c496
Adhi venkatesh <adhivenkatesh32@gmail.com>
2021-05-22 12:31:09 +0530

    changes-git for merge

--- Program.cs
+++ Program.cs
@@ -7,12 +7,7 @@ namespace ConsoleAppGitCheck
         static void Main(string[] args)
         {
             //code changes in the branch  -Develop7-BrachCreatedFromLocalMaster
-            Console.WriteLine("Hello World change from MODIFIED -Data from develop-0");
-            Console.WriteLine("Branch7 changes done from github4"  );
-            Console.WriteLine("Branch7 changes done from github3"  );
-            Console.WriteLine("Branch7 changes done from github2"  );
-            Console.WriteLine("Branch7 changes done from github1"  );
-            Console.WriteLine("Branch7 changes done from github0"  );
+           
         }
     }
 }
